04c22edd68d4a608863da753871bf0481475c4c4
Sven Soldin <soldinsven@gmail.com>
2020-10-01 14:02:03 +0200

    test

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json());
 //Define routes
 app.use("/pictures", require("./api/routes/pictures"));
 app.use("/users", require("./api/routes/users"));
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
