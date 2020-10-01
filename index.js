25431b30b1fda742d2261b0d8a8d6a3a8b94cade
Sven Soldin <soldinsven@gmail.com>
2020-10-01 12:49:22 +0200

    stored mongo string in env variable, allowed all CORS

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Origin",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 // connect DB
 mongoose.set("useUnifiedTopology", true);
-mongoose.connect(
-	`mongodb+srv://kikoplou:${process.env.PASSWORD}@mern.yy0tj.mongodb.net/splashunDB?retryWrites=true&w=majority`,
-	{ useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(express.json());
