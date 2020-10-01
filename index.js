e40a21d96f0684a4a477ad4a5270610dfae2ef6a
Sven Soldin <soldinsven@gmail.com>
2020-10-01 14:13:02 +0200

    imported cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const corsOptions = {
