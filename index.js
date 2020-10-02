ce77374c3ca55e0d09c6a5ead359115b0c7c68b3
Sven Soldin <soldinsven@gmail.com>
2020-10-02 13:04:46 +0200

    cors

--- index.js
+++ index.js
@@ -5,20 +5,13 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "https://splashun.netlify.app"); // update to match the domain you will make the request from
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
+app.use(cors());
+app.use(express.json());
+
 // connect DB
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
-
-app.use(express.json());
 
 //Define routes
 app.use("/pictures", require("./api/routes/pictures"));
